d6a21d441cb070e27c644ed0890a284f322ca3fb
SpringDL <DanL4@live.ca>
2015-02-03 19:40:12 -0500

    Movement Updates #9

--- Assets/Resources/Scripts/PlayerMovement.cs
+++ Assets/Resources/Scripts/PlayerMovement.cs
@@ -4,16 +4,24 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float speed = 500;
+	private GameObject p8;
+	private Vector3 p8position = new Vector3(0, 0, 0);
+	public bool isMoving = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		p8 = GameObject.Find("P8");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.W)){
-			rigidbody.AddForce(transform.forward * speed * Time.deltaTime);
+			if (!isMoving){
+				isMoving = true;
+				p8position = p8.transform.position;
+				StartCoroutine("MoveForwards");
+			}
+			//rigidbody.AddForce(transform.forward * speed * Time.deltaTime);
 		}
 
 		if (Input.GetKeyDown(KeyCode.A)){
@@ -22,6 +30,20 @@ public class PlayerMovement : MonoBehaviour {
 		if (Input.GetKeyDown(KeyCode.D)){
 			StartCoroutine("RotateRight");
 		}
+
+	}
+
+	IEnumerator MoveForwards () {
+		do{
+			rigidbody.AddForce(transform.forward * speed * Time.deltaTime);
+			Debug.Log("Am Boofy");
+			yield return null;
+		}while(transform.position.z <= p8position.z);
+		if (!(Input.GetKey(KeyCode.W))){
+			rigidbody.velocity = new Vector3(0, 0, 0);
+		}
+		transform.position = new Vector3(Mathf.Round(transform.position.x), transform.position.y, Mathf.Round(transform.position.z));
+		isMoving = false;
 	}
 
 	IEnumerator RotateLeft () {
---
+++ Assets/Resources/Scripts/p8.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class p8 : MonoBehaviour {
+
+	private GameObject player;
+	public float offset = 1;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + offset);
+	}
+}
