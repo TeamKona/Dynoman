bf7a669c00ba5c56aea90132b9fcb0638608b827
ryans3 <ryan.s3@hotmail.com>
2015-02-25 18:02:51 -0500

    ScnBomblobber
    Bomb Spawns cubes

--- Assets/Resources/Scripts/Bomb.cs
+++ Assets/Resources/Scripts/Bomb.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class Bomb : MonoBehaviour {
 
+
+
+	public float bombFuse = 2f;
+	public GameObject spawnExplosion;
+
+
 	// Use this for initialization
 	void Start () {
 	
+		StartCoroutine("BombTimer");
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+
 	}
+
+	IEnumerator BombTimer (){
+
+		Debug.Log("whatever");
+		yield return new WaitForSeconds(bombFuse);
+		GameObject.Find("Player").GetComponent<SpawnBomb>().amount--;
+		GameObject.Destroy(this.gameObject);
+		Instantiate(spawnExplosion,new Vector3 (transform.position.x,transform.position.y,transform.position.z+1), transform.rotation);
+		//Instantiate(part1, part2, part3);
+	}
+
+
+
 }
---
+++ Assets/Resources/Scripts/SpawnBomb.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnBomb : MonoBehaviour {
+
+	bool canBomb = true;
+	public GameObject bomb;
+	public int amount = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown(KeyCode.Space)){
+			
+			SpawnBomber();
+
+		}
+
+
+
+	}
+
+	void SpawnBomber()
+	{
+		if (amount <= 3 && canBomb)
+		{
+
+			Instantiate(bomb,transform.position, transform.rotation);
+			amount++;
+			canBomb = false;
+			StartCoroutine("BombCooldown");
+
+		}
+		else if (amount > 3)
+		{
+			Debug.Log("Too many bombs");
+		}
+	}
+	void OnGUI()
+	{
+		GUI.Box(new Rect(Screen.width / 2 - 300, Screen.width / 2 - 400, 50, 30), amount.ToString());
+	}
+
+
+	IEnumerator BombCooldown (){
+
+		yield return new WaitForSeconds(0.75f);
+		canBomb = true;
+	}
+	
+
+}
